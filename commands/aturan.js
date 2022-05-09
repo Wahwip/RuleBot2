1bd94bdc6b4deda1b0d0088e41f9adc073eba249
User3 <user3@Tests-iMac.local>
2022-05-09 20:24:42 +0700

    first commit

---
+++ commands/aturan.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'aturan',
+    description: "Hanya untuk bersenang senang",
+    execute(message, args, Discord) {
+       message.channel.send('Silahkan membaca <#840149894273695745>') 
+    }
+}
